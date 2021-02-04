2eb1fea164eb82b71a846f65968b9a5d4c0cb260
Felix-Sommer-BHS <FSommer@bhs-world.com>
2021-02-04 13:34:53 +0100

    readability error

--- FileDispatcher.cs
+++ FileDispatcher.cs
@@ -3,12 +3,13 @@ using System.Windows;
 
 namespace Testing
 {
-    public enum test
+    /// <summary>
+    /// test
+    /// </summary>
+    public enum TestEnum
     {
         ABC, DEF,
     }
-
-    ValueTuple<int, int> x;
 
     /// <summary>
     /// here we do.
@@ -24,16 +25,16 @@ namespace Testing
                 _i,
             _j;
 
-        private ITranslator _translator;
-
+        
 
         public FileDispatcher(Config config)
         {
             _config = config; ;
         }
 
-        public event EventHandler<ConveyData> DecodeCompleted;
-
+        /// <summary>
+        /// ffrfe
+        /// </summary>
         public void Init(
             )
         {
@@ -42,40 +43,13 @@ namespace Testing
 
             switch (_config.TypeTranslator) //comment
             {
-                case TranslatorKind.V_2_7:
-                    {
-                        _translator = new Translatorv2_7();
-                        break;
-                    }
-
                 case TranslatorKind.V_2_8:
                     _translator = new Translatorv2_8();
-                    break;
-            }
-
-            switch (_config.TypeInterface)
-            {
-                case DeviceKind.FileTransfer:
-                    _device = new FileDevice(_config);
-                    _device.ProcessCompleted += StartTranslator;
-                    _device.Init();
-                    break;
-
-                case DeviceKind.WinSocket:
-                    _device = new WinSocketDevice(_config);
-                    _device.ProcessCompleted += StartTranslator;
-                    _device.Init();
-
-                    break;
-
-                default:
-                    MessageBox.Show("File Device does not exist");
                     break;
             }
         }
 
         public void StartTranslator
-
 
             (
 
@@ -86,52 +60,7 @@ namespace Testing
         {
             if (null != DecodeCompleted)
             {
-                // File Transfer daten fehlen noch!!!!!!!!
-                ConveyData c1 = _translator.TranslateInformation(dataWpa);
-                c1.DeviceKind = _config.TypeInterface;
-                c1.TranslatorTyp = _config.TypeTranslator;
-                c1.ConnectionTyp = _config.ConnectionTyp;
-                switch (_config.TypeInterface)
-                {
-                    case DeviceKind.FileTransfer:
-                        c1.FilePath = _config.PathFileTransfer;
-                        c1.Port = 0;
-                        c1.Ip = "nur für WinSocket";
-
-                        break;
-
-                    case DeviceKind.WinSocket:
-                        c1.FilePath = "Nur für FileTransfer";
-                        switch (_config.ConnectionTyp)
-                        {
-                            case ConnectionKind.Server:
-                                c1.Port = _config.PortServer;
-                                c1.Ip = _config.IpServer;
-                                break;
-
-                            case ConnectionKind.Client:
-                                c1.Port = _config.PortClient;
-                                c1.Ip = _config.IpClient;
-                                break;
-                        }
-
-                        break;
-                }
-
-                switch (this._config.ConnectionTyp)
-                {
-                    case ConnectionKind.Server:
-                        c1.Port = _config.PortServer;
-                        c1.Ip = _config.IpServer;
-                        break;
-
-                    case ConnectionKind.Client:
-                        c1.Port = _config.PortClient;
-                        c1.Ip = _config.IpClient;
-                        break;
-                }
-
-                DecodeCompleted(this, c1);
+                return;
             }
         }
     }
